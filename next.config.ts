c82b67b96b521c45182d94aba8eeae3c37739042
sonne-wat <sonneleuchtet@gmail.com>
2026-03-28 20:36:00 +0900

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
+    // 빌드 시 타입 오류가 있어도 무시하고 배포함
     ignoreBuildErrors: true,
   },
   eslint: {
+    // 빌드 시 코드 규칙 오류가 있어도 무시하고 배포함
     ignoreDuringBuilds: true,
   },
 };
